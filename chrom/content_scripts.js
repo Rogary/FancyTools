c115e965598bb42f663a6f447897c753fcc1c7fc
rogary <rogaryljw@gmail.com>
2017-10-24 18:12:25 +0800

    获取页面数据，并使用正则获取链接地址

--- chrom/content_scripts.js
+++ chrom/content_scripts.js
@@ -1,7 +1,22 @@
-chrome.tabs.onCreated.addListener(function(tab) {
-  　　　　console.log('tabs.onCreated --'
-              + ' window: ' + tab.windowId
-              + ' tab: '    + tab.id
-              + ' index: '  + tab.index
-              + ' url: '    + tab.url);
-});
+var html = document.body.innerHTML;
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendMessage) {if (request.greeting == "hello"){
+            var regx = new RegExp('[a-zA-z]+://[^\s\"]*','g');
+            var i = 0;
+            var results=new Array();
+            console.log(html);
+            do
+            {
+              result=regx.exec(html);
+              if (result!=null) {
+                results[i]=result;
+                i++;
+              }
+              console.log(result);
+            }while (result!=null)
+            sendMessage(results);
+          }
+        else{
+            sendMessage("FUCK OFF"); // snub them.
+          }
+    });
--- chrom/sample.js
+++ chrom/sample.js
@@ -1,20 +1,19 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("request " + request);
-	console.log("sender " + sender);
-	console.log("sendResponse " + sendResponse);
-});
-
-// A generic onclick callback function.
 function genericOnClick(info, tab) {
-  console.log("select " + chrome.tabs.getSelected);
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
-  console.log("page：" + document.documentElement.outerHTML);
+  // 向content-script.js发送请求信息
+  chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(response) {
+      console.log(response.toString());　　
+      var clipboard = new Clipboard(response.toString());//实例化
+      clipboard.on('success', function(e) {
+          console.log(e);
+          e.clearSelection();
+      });
+
+      clipboard.on('error', function(e) {
+          console.log(e);
+      });
+  });
 }
 var id = chrome.contextMenus.create({"title": "复制所有下载链接", "contexts":["page"],
-								   "onclick": genericOnClick});
+                   "onclick": genericOnClick});
 console.log("'" + "' item:" + id);
